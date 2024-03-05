1fbcdaa9539643e212e063817174f7d6cfa4ee98
승찬 <anstmdcks12@naver.com>
2024-03-05 22:23:37 +0900

    feat: 디자인 시스템 리소스 추가

--- Project/Core/DesignSystem/Sources/DesignSystem/Component/DesignSystemColor/DesignSystemColor.swift
+++ Project/Core/DesignSystem/Sources/DesignSystem/Component/DesignSystemColor/DesignSystemColor.swift
@@ -11,19 +11,19 @@ public enum DesignSystemColor: Equatable, CaseIterable {
   case tint(TintChip)
   case error(ErrorChip)
   case overlay(OverlayChip)
-
+  
   // MARK: Public
-
+  
   public static var allCases: [Self] {
     BackgroundChip.allCases.map(Self.background)
-      + LabelChip.allCases.map(Self.label)
-      + PaletteChip.allCases.map(Self.palette)
-      + SystemChip.allCases.map(Self.system)
-      + TintChip.allCases.map(Self.tint)
-      + ErrorChip.allCases.map(Self.error)
-      + OverlayChip.allCases.map(Self.overlay)
+    + LabelChip.allCases.map(Self.label)
+    + PaletteChip.allCases.map(Self.palette)
+    + SystemChip.allCases.map(Self.system)
+    + TintChip.allCases.map(Self.tint)
+    + ErrorChip.allCases.map(Self.error)
+    + OverlayChip.allCases.map(Self.overlay)
   }
-
+  
   public var color: Color {
     switch self {
     case .background(let chip): chip.color
@@ -38,46 +38,42 @@ public enum DesignSystemColor: Equatable, CaseIterable {
 }
 
 extension DesignSystemColor {
-
+  
   public enum BackgroundChip: Equatable, CaseIterable {
     case green
-    case sky
-
+    case blue
+    
     var color: Color {
       switch self {
       case .green: Asset.ColorChip.Background.backgroundGreen.swiftUIColor
-      case .sky: Asset.ColorChip.Background.backgroundSky.swiftUIColor
+      case .blue: Asset.ColorChip.Background.backgroundBlue.swiftUIColor
       }
     }
   }
-
+  
   public enum LabelChip: Equatable, CaseIterable {
     case `default`
-    case gradient100
-    case gradient200
-
+    
     var color: Color {
       switch self {
-      case .`default`: Asset.ColorChip.Label.labelDefault.swiftUIColor
-      case .gradient100: Asset.ColorChip.Label.labelGradient100.swiftUIColor
-      case .gradient200: Asset.ColorChip.Label.labelGradient200.swiftUIColor
+      case .default: Asset.ColorChip.Label.labelDefault.swiftUIColor
       }
     }
   }
-
+  
   public enum PaletteChip: Equatable, CaseIterable {
-
+    
     case gray(GrayChip)
-
+    
     // MARK: Public
-
+    
     public enum GrayChip: Equatable, CaseIterable {
       case lv100
       case lv200
       case lv250
       case lv300
       case lv400
-
+      
       var color: Color {
         switch self {
         case .lv100: Asset.ColorChip.Palette.Gray.paletteGray100.swiftUIColor
@@ -88,26 +84,26 @@ extension DesignSystemColor {
         }
       }
     }
-
+    
     public static var allCases: [DesignSystemColor.PaletteChip] {
       PaletteChip.GrayChip.allCases.map(PaletteChip.gray)
     }
-
+    
     // MARK: Internal
-
+    
     var color: Color {
       switch self {
       case .gray(let chip): chip.color
       }
     }
-
+    
   }
-
+  
   public enum SystemChip: Equatable, CaseIterable {
-
+    
     case black
     case white
-
+    
     var color: Color {
       switch self {
       case .black: Asset.ColorChip.System.systemBlack.swiftUIColor
@@ -115,31 +111,35 @@ extension DesignSystemColor {
       }
     }
   }
-
+  
   public enum TintChip: Equatable, CaseIterable {
-
-    case `default`
-
+    
+    case sand
+    case purple
+    case green
+    
     var color: Color {
       switch self {
-      case .default: Asset.ColorChip.Tint.tintDefault.swiftUIColor
+      case .sand: Asset.ColorChip.Tint.tintSand.swiftUIColor
+      case .purple: Asset.ColorChip.Tint.tintPurple.swiftUIColor
+      case .green: Asset.ColorChip.Tint.tintGreen.swiftUIColor
       }
     }
   }
-
+  
   public enum ErrorChip: Equatable, CaseIterable {
     case `default`
-
+    
     var color: Color {
       switch self {
       case .default: Asset.ColorChip.Error.errorDefault.swiftUIColor
       }
     }
   }
-
+  
   public enum OverlayChip: Equatable, CaseIterable {
     case `default`
-
+    
     var color: Color {
       switch self {
       case .default: Asset.ColorChip.Overlay.overlayDefault.swiftUIColor
