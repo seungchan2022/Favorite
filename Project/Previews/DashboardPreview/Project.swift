62cae308920ec8976cfe3362825faf587c6bc566
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2024-04-21 20:26:03 +0900

    chore: Tuist 4.9.0 마이그레이션

--- Project/Previews/DashboardPreview/Project.swift
+++ Project/Previews/DashboardPreview/Project.swift
@@ -4,12 +4,12 @@ import ProjectDescriptionHelpers
 let project: Project = .preview(
   projectName: "Dashboard",
   packages: [
-    .local(path: "../../../Modules/Core/Architecture"),
-    .local(path: "../../../Modules/Core/DesignSystem"),
-    .local(path: "../../../Modules/Core/Domain"),
-    .local(path: "../../../Modules/Core/Platform"),
-    .local(path: "../../../Modules/Core/Functor"),
-    .package(path: "../../../Modules/Feature/Dashboard"),
+    .local(path: .relativeToRoot("Modules/Core/Architecture")),
+    .local(path: .relativeToRoot("Modules/Core/DesignSystem")),
+    .local(path: .relativeToRoot("Modules/Core/Domain")),
+    .local(path: .relativeToRoot("Modules/Core/Platform")),
+    .local(path: .relativeToRoot("Modules/Core/Functor")),
+    .local(path: .relativeToRoot("Modules/Feature/Dashboard")),
   ],
   dependencies: [
     .package(product: "Dashboard", type: .runtime)
